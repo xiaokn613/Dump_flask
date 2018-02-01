faccc28b162c11d226421c26c8ea98d7fe1e6d90
xiaokn613 <460559680@qq.com>
2018-02-01 11:45:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,24 @@ def hello_world():
     except:
         return str({"error_caseId":caseId})
 
+
+@app.route('/doc')
+def hello_doc():
+    doc_id = request.args.get("doc_id")
+    url = 'http://mobile-api.haodf.com/patientapi/doctor_getCaseListByDoctorIdNew'
+    headers = {
+    	'User-Agent':'haodf_app/1.0',
+	'Content-Type':'application/x-www-form-urlencoded',
+	'Host':'mobile-api.haodf.com',
+	'Connection':'Keep-Alive',
+	'Accept-Encoding':'gzip'
+    }
+    post_data = 'n=2&s=xm&userId=0&api=1.2&m=MI%205&app=p&os=android&sv=7.0&di=862033036085049&v=5.6.1&deviceToken=862033036085049&p=1&doctorId={doc_id}&nowPage=1&_t=c42f4825f57f500d71a28a2fe2a16899&currentUserId=0'.format(doc_id=doc_id)
+    try:
+	res = requests.post(url,data=post_data,headers=headers)
+	return str(res.text)
+    except:
+	return str({"error_caseId":doc_id})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=2333)
